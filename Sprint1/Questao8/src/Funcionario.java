f5e31f0c698bde9088ad43389478f66cfa2f9d08
Rafael <rafinhalq@gmail.com>
2022-10-14 21:46:16 -0300

    Comentario e formatação de Funcionario.java

--- Sprint1/Questao8/src/Funcionario.java
+++ Sprint1/Questao8/src/Funcionario.java
@@ -23,19 +23,21 @@ public class Funcionario {
         this.salario = salario;
     }
 
+    // Este método recebe a porcentagem como argumento para acrescentar ao salário.
     public double acrescentaEmPorcentagem(double porcentagem) {
         double salarioAntigo = this.salario;
         double bonus;
-        porcentagem = 1 + porcentagem/100;
+        porcentagem = 1 + porcentagem / 100;
         this.salario *= porcentagem;
         bonus = this.salario - salarioAntigo;
         return bonus;
     }
 
+    // Este método recebe a porcentagem como argumento para descontar do salário.
     public double descontaEmPorcentagem(double porcentagem) {
         double salarioAntigo = this.salario;
         double desconto;
-        porcentagem = 1 - porcentagem/100;
+        porcentagem = 1 - porcentagem / 100;
         this.salario *= porcentagem;
         desconto = salarioAntigo - this.salario;
         return desconto;
