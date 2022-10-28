d947159ca01f5c7e34d881e5b590600b16b6b598
Rafael <rafinhalq@gmail.com>
2022-10-27 21:44:31 -0300

    Adicionando os métodos a classe Produto

--- Sprint2/Questao1/src/Produto.java
+++ Sprint2/Questao1/src/Produto.java
@@ -6,4 +6,30 @@ public class Produto {
     private int quantidade;
     private double preco;
     
+    public Produto(String nome, String descricao, int quantidade, double preco) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
 }
