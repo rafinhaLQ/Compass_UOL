793ce6c79615b6bb91db18d335b148a210f55e9f
Rafael <rafinhalq@gmail.com>
2022-11-13 14:57:48 -0300

    Criação do método lista()

--- Sprint3/Questao1/estados/src/main/java/uol/compass/estados/controller/EstadoController.java
+++ Sprint3/Questao1/estados/src/main/java/uol/compass/estados/controller/EstadoController.java
@@ -2,11 +2,16 @@ package uol.compass.estados.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import uol.compass.estados.controller.dto.EstadoDto;
+import uol.compass.estados.model.Estado;
 import uol.compass.estados.repository.EstadoRepository;
 
 @RestController
@@ -17,7 +22,15 @@ public class EstadoController {
     private EstadoRepository estadoRepository;
 
     @GetMapping
-    public Page<EstadoDto> lista() {
+    public Page<EstadoDto> lista(@RequestParam(required = false) String regiao, 
+        @PageableDefault(sort = {"populacao", "area"}, direction = Direction.DESC) Pageable paginacao) {
         
+        if (regiao==null) {
+            Page<Estado> estados = estadoRepository.findAll(paginacao);
+            return EstadoDto.converter(estados);
+        } else {
+            Page<Estado> estados = estadoRepository.findByRegiao(regiao, paginacao);
+            return EstadoDto.converter(estados);
+        }
     }
 }
