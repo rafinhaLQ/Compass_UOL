611c2ef10f1e453e8158e3810af09a0a7ec1972a
Rafael <rafinhalq@gmail.com>
2022-11-13 11:52:15 -0300

    Criação do enum Região

--- Sprint3/Questao1/estados/src/main/java/uol/compass/estados/model/Estado.java
+++ Sprint3/Questao1/estados/src/main/java/uol/compass/estados/model/Estado.java
@@ -5,4 +5,10 @@ import javax.persistence.Entity;
 @Entity
 public class Estado {
     
+    private Long id;
+    private String nome;
+    private Regiao regiao;
+    private Long população;
+    private String capital;
+    private Double area; 
 }
---
+++ Sprint3/Questao1/estados/src/main/java/uol/compass/estados/model/Regiao.java
@@ -0,0 +1,11 @@
+package uol.compass.estados.model;
+
+public enum Regiao {
+
+    Norte,
+    Nordeste,
+    Sul,
+    Sudeste,
+    Centro_Oeste
+
+}
