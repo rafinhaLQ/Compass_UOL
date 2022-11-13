23170fc4d912c21cd9e75ec1c1a6aae675a0e0d3
Rafael <rafinhalq@gmail.com>
2022-11-13 12:00:16 -0300

    Anotações da classe Estado

--- Sprint3/Questao1/estados/src/main/java/uol/compass/estados/model/Estado.java
+++ Sprint3/Questao1/estados/src/main/java/uol/compass/estados/model/Estado.java
@@ -1,14 +1,21 @@
 package uol.compass.estados.model;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Estado {
     
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    @Enumerated(EnumType.STRING)
     private Regiao regiao;
     private Long população;
     private String capital;
-    private Double area; 
+    private Double area;
 }
