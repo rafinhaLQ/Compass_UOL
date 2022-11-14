e56bfa55d5a2a27fa49185131eb6f23f974f0db6
Rafael <rafinhalq@gmail.com>
2022-11-14 12:05:46 -0300

    Adicionando o atributo capital ao endpoint Put

--- Sprint3/Questao1/estados/src/main/java/uol/compass/estados/controller/form/AtualizacaoEstadoForm.java
+++ Sprint3/Questao1/estados/src/main/java/uol/compass/estados/controller/form/AtualizacaoEstadoForm.java
@@ -1,12 +1,17 @@
 package uol.compass.estados.controller.form;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
 
 import uol.compass.estados.model.Estado;
 import uol.compass.estados.repository.EstadoRepository;
 
 public class AtualizacaoEstadoForm {
     
+    @NotNull @NotEmpty @Length(min = 5)
+    private String capital;
     @NotNull
     private Long populacao;
 
@@ -18,9 +23,19 @@ public class AtualizacaoEstadoForm {
         this.populacao = populacao;
     }
 
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
     public Estado atualizar(Long id, EstadoRepository estadoRepository) {
         Estado estado = estadoRepository.getReferenceById(id);
         estado.setPopulacao(populacao);
+        estado.setCapital(capital);
         return estado;
     }
+    
 }
