998b0f7f6a8b1d2abc730aa692de3b4ac0ee7b86
Rafael <rafinhalq@gmail.com>
2022-11-23 18:12:53 -0300

    Adicionando classe Partido

---
+++ Sprint4/sistemaPolitico/src/main/java/uol/compass/sistemaPolitico/model/Partido.java
@@ -0,0 +1,13 @@
+package uol.compass.sistemapolitico.model;
+
+import java.util.Date;
+
+public class Partido {
+    
+    private Long id;
+    private String nome;
+    private String sigla;
+    private Ideologia ideologia;
+    private Date dataFundacao;
+
+}
