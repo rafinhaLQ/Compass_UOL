55ae2271aadcd0fcc7703c9b1a3da452edc8b644
Rafael <rafinhalq@gmail.com>
2022-11-23 18:11:43 -0300

    Trocando aplicacao spring boot

--- Sprint4/sistemaPolitico/src/main/java/uol/compass/sistemaPolitico/SistemaPoliticoApplication.java
+++ Sprint4/sistemaPolitico/src/main/java/uol/compass/sistemaPolitico/SistemaPoliticoApplication.java
@@ -1,13 +1,13 @@
-package uol.compass.sistemaPolitico;
+package uol.compass.sistemapolitico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SistemaPoliticoApplication {
+public class SistemapoliticoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SistemaPoliticoApplication.class, args);
+		SpringApplication.run(SistemapoliticoApplication.class, args);
 	}
 
 }
--- Sprint4/sistemaPolitico/src/test/java/uol/compass/sistemaPolitico/SistemaPoliticoApplicationTests.java
+++ Sprint4/sistemaPolitico/src/test/java/uol/compass/sistemaPolitico/SistemaPoliticoApplicationTests.java
@@ -1,10 +1,10 @@
-package uol.compass.sistemaPolitico;
+package uol.compass.sistemapolitico;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SistemaPoliticoApplicationTests {
+class SistemapoliticoApplicationTests {
 
 	@Test
 	void contextLoads() {
