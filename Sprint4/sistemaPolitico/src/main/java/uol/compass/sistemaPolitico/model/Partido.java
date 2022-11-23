c2c7aa74204ace978e471e16e694a6a3f28f4e4f
Rafael <rafinhalq@gmail.com>
2022-11-23 18:20:40 -0300

    Adicionando anotacoes da JPA

--- Sprint4/sistemaPolitico/src/main/java/uol/compass/sistemaPolitico/model/Partido.java
+++ Sprint4/sistemaPolitico/src/main/java/uol/compass/sistemaPolitico/model/Partido.java
@@ -1,13 +1,23 @@
 package uol.compass.sistemapolitico.model;
 
-import java.util.Date;
+import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Partido {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String sigla;
+    @Enumerated
     private Ideologia ideologia;
-    private Date dataFundacao;
+    private LocalDate dataFundacao;
 
 }
