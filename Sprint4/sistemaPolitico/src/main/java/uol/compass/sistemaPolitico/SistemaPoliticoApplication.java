f9ace663adfa9725864c164099fcb566caa763f1
Rafael <rafinhalq@gmail.com>
2022-11-23 17:55:59 -0300

    Criando a aplicação

---
+++ Sprint4/sistemaPolitico/src/main/java/uol/compass/sistemaPolitico/SistemaPoliticoApplication.java
@@ -0,0 +1,13 @@
+package uol.compass.sistemaPolitico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPoliticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPoliticoApplication.class, args);
+	}
+
+}
---
+++ Sprint4/sistemaPolitico/src/test/java/uol/compass/sistemaPolitico/SistemaPoliticoApplicationTests.java
@@ -0,0 +1,13 @@
+package uol.compass.sistemaPolitico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPoliticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
