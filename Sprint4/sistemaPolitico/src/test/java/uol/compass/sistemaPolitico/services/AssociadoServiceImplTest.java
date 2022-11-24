cf7d076f48e5a7c1226dea2272e24cae2e66fef6
Rafael <rafinhalq@gmail.com>
2022-11-24 06:32:43 -0300

    Criando classe de teste para desenvolvimento tdd

---
+++ Sprint4/sistemaPolitico/src/test/java/uol/compass/sistemaPolitico/services/AssociadoServiceImplTest.java
@@ -0,0 +1,14 @@
+package uol.compass.sistemapolitico.services;
+
+import org.junit.jupiter.api.Test;
+
+public class AssociadoServiceImplTest {
+    
+    
+
+    @Test
+    void deveriaCriarAssociadoComSucesso() {
+
+    }
+
+}
