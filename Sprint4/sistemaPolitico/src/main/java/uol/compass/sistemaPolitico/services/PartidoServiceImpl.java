15acec7e0f6064291443d6190bf2e37b6ee183bf
Rafael <rafinhalq@gmail.com>
2022-11-25 07:50:34 -0300

    Criando método cadastrar

--- Sprint4/sistemaPolitico/src/main/java/uol/compass/sistemaPolitico/services/PartidoServiceImpl.java
+++ Sprint4/sistemaPolitico/src/main/java/uol/compass/sistemaPolitico/services/PartidoServiceImpl.java
@@ -1,17 +1,30 @@
 package uol.compass.sistemapolitico.services;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
 import uol.compass.sistemapolitico.dto.pedido.PartidoPedidoDto;
 import uol.compass.sistemapolitico.dto.resposta.PartidoParametrosResposta;
 import uol.compass.sistemapolitico.dto.resposta.PartidoRespostaDto;
+import uol.compass.sistemapolitico.entidades.Partido;
+import uol.compass.sistemapolitico.repository.PartidoRepository;
 
+@Service
+@RequiredArgsConstructor
 public class PartidoServiceImpl implements PartidoService {
+
+    private final ModelMapper modelMapper;
+
+    private final PartidoRepository partidoRepository;
 
     @Override
     public PartidoRespostaDto cadastra(PartidoPedidoDto pedido) {
-        // TODO Auto-generated method stub
-        return null;
+        Partido paraCriar = modelMapper.map(pedido, Partido.class);
+        Partido criado = partidoRepository.save(paraCriar);
+
+        return modelMapper.map(criado, PartidoRespostaDto.class);
     }
 
     @Override
