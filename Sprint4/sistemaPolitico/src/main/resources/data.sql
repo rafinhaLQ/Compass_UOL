5626d56873d1fb44119eeaee60fe8f9cfb870ee4
Rafael <rafinhalq@gmail.com>
2022-11-26 17:20:30 -0300

    Inicializando dados no banco

---
+++ Sprint4/sistemaPolitico/src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO associados(`nome`, `cargo_politico`, `data_nascimento`, `sexo`)
+VALUES('Bruno Reis', 'PREFEITO', '1977-05-17', 'MASCULINO');
+INSERT INTO associados(`nome`, `cargo_politico`, `data_nascimento`, `sexo`)
+VALUES('Kim Kataguiri', 'DEPUTADO_FEDERAL', '1996-01-28', 'MASCULINO');
+INSERT INTO associados(`nome`, `cargo_politico`, `data_nascimento`, `sexo`)
+VALUES('Rui Costa', 'GOVERNADOR', '1963-01-18', 'MASCULINO');
+INSERT INTO associados(`nome`, `cargo_politico`, `data_nascimento`, `sexo`)
+VALUES('Dilma Rousseff', 'PRESIDENTE', '1947-12-14', 'FEMININO');
+INSERT INTO associados(`nome`, `cargo_politico`, `data_nascimento`, `sexo`)
+VALUES('Simone Tebet', 'SENADOR', '1970-02-22', 'FEMININO');
+
+INSERT INTO partidos(`nome`, `sigla`, `ideologia`, `data_fundacao`)
+VALUES('Partido dos Trabalhadores', 'PT', 'ESQUERDA', '1982-02-11');
+INSERT INTO partidos(`nome`, `sigla`, `ideologia`, `data_fundacao`)
+VALUES('União Brasil', 'UNIAO', 'DIREITA', '2022-02-08');
+INSERT INTO partidos(`nome`, `sigla`, `ideologia`, `data_fundacao`)
+VALUES('Partido Verde', 'PV', 'CENTRO', '1993-09-30');
